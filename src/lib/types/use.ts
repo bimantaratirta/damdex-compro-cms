8e0978bbe660aac8a0afa0cf4c9c79628dfdf35c
Arziki Pratama <arzi1605@gmail.com>
2025-06-01 23:37:24 +0700

    fix: minor fix

--- src/lib/types/use.ts
+++ src/lib/types/use.ts
@@ -9,7 +9,7 @@ export interface Use extends Base {
 
 export interface UseFor extends Base {
   useComposition: string;
-  useCompositionid: number;
+  useCompositionId: number;
   titleIDN: string;
   titleENG: string;
   descriptionIDN: string;
